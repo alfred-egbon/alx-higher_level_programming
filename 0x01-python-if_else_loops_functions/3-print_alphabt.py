e3fa7b42f1b71d06f14e2ac027f5f0d27a473798
alfred-egbon <egbon.o.a@gmail.com>
2022-12-02 06:00:41 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # Author - Alfred Egbon
 forletter in range(97, 123):
-    if chr(letter) != 'q' 'e':
+    if chr(letter) != 'q':
             print("{}".format(chr(letter)), end="")
